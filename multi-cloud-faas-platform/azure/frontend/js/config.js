7b499a709966c0d546cdbc9aecce4c5df21e9335
Shaomin Zhou <szho0136@student.monash.edu>
2026-05-18 10:19:19 +1000

    feat: configure Cognito auth for Azure Static Web App

---
+++ multi-cloud-faas-platform/azure/frontend/js/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  cognitoDomain: "https://shaomin-faas-dev.auth.us-east-1.amazoncognito.com",
+  clientId: "2ffrs1pe6rlkb6jf2mpopbqhfq",
+
+  redirectUri: "https://blue-wave-0c8589a0f.7.azurestaticapps.net/callback",
+  logoutUri: "https://blue-wave-0c8589a0f.7.azurestaticapps.net/",
+
+  appUrl: "/app.html"
+};
